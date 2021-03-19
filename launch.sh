5f77064293da093cd587dcac63e03102006103c8
APVGITHUB <49819845+APVGITHUB@users.noreply.github.com>
2021-03-19 15:39:39 +0100

    Update launch.sh
    modified rostopic name

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 source ./devel/setup.bash
 roslaunch hector_gazebo_worlds rescue_mission.launch &
 roslaunch hector_quadrotor_gazebo spawn_quadrotor_with_thermal.launch &
-rosrun image_view image_view image:=/thermal_camera/image_raw &
+rosrun image_view image_view image:=/thermal_cam/camera/image &
 sleep 20
 echo "enabling motors"
 rosservice call /enable_motors true
